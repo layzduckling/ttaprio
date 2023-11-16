4ffc9fdf734aa96166d41e0b4b26196f5dd693be
layzduckling <layzduckling@icloud.com>
2023-11-16 20:30:46 +0900

    Change font from mono to sans

--- client/components/Logo/index.jsx
+++ client/components/Logo/index.jsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className="relative flex items-center w-screen h-[48px] bg-[#212A3E] z-50">
       <div className="absolute left-6 text-2xl text-[#F1F6F9] font-bold">
-        <h1>&lt;ttapr.io&gt;</h1>
+        <h1>ttapr.io</h1>
       </div>
     </div>
   );
