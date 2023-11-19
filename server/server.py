3f94e90c53fc635f3f809e3a3882fb39c9e9827c
layzduckling <layzduckling@icloud.com>
2023-11-19 15:54:03 +0900

    Add eventlet to requirements for gunicorn

--- server/server.py
+++ server/server.py
@@ -124,5 +124,5 @@ def handle_eval(data):
 instruction = ""
 rubric = ""
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
+# if __name__ == "__main__":
+#     socketio.run(app, host="0.0.0.0", port=8080, debug=True)
