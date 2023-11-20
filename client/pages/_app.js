6fe5844c186f571f2b08f1be9337d7d08e0500be
layzduckling <layzduckling@icloud.com>
2023-11-20 19:29:34 +0900

    Add favicon and title that will appear on the tab

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,14 @@
 import "../styles/global.css";
 import "./ql.css";
 
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+  <Head>
+    <title>ttaprio</title>
+    <link rel="icon" href="/images/bird_icon.png" />
+  </Head>
+  <Component {...pageProps} />
+  </>;
 }
